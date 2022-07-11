d44297cb36aa78bb04d3f66760797defd704ccdf
onmidnightblue <wavesofdawn@nate.com>
2022-07-11 18:03:43 +0900

    firebase deploy

--- redux/02.cart/src/store/cart-actions.js
+++ redux/02.cart/src/store/cart-actions.js
@@ -5,7 +5,7 @@ export const fetchCartData = () => {
   return async (dispatch) => {
     const fetchData = async () => {
       const response = await fetch(
-        "https://redux-start-4c276-default-rtdb.firebaseio.com/cart.json"
+        "https://redux-2ec89-default-rtdb.firebaseio.com/cart.json"
       );
 
       if (!response.ok) {
@@ -49,7 +49,7 @@ export const sendCartData = (cart) => {
 
     const sendRequest = async () => {
       const response = await fetch(
-        "https://redux-start-4c276-default-rtdb.firebaseio.com/cart.json",
+        "https://redux-2ec89-default-rtdb.firebaseio.com/cart.json",
         {
           method: "PUT",
           body: JSON.stringify({
