e1d5b33fa32edb42f2869b9f214b4bd9d8c1f5b8
onmidnightblue <wavesofdawn@nate.com>
2022-08-24 18:00:05 +0900

    2022-08-24 algorithm

--- algorithm/algorithm-dataStructure/04.Sorting/33.bubbleSort.js
+++ algorithm/algorithm-dataStructure/04.Sorting/33.bubbleSort.js
@@ -1,7 +1,6 @@
 // function bubbleSort(arr) {
 //   for (let i = 0; i < arr.length; i++) {
 //     for (let j = 0; j < arr.length; j++) {
-//       console.log(arr, arr[j], arr[j + 1]);
 //       if (arr[j] > arr[j + 1]) {
 //         let temp = arr[j];
 //         arr[j] = arr[j + 1];
@@ -12,52 +11,41 @@
 //   return arr;
 // }
 
-// bubbleSort([37, 45, 29, 8]);
+// function bubbleSort(arr) {
+//   for (let i = arr.length; i > 0; i--) {
+//     for (let j = 0; j < i - 1; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+//   return arr;
+// }
 
-function bubbleSort1(arr) {
-  for (let i = arr.length; i > 0; i--) {
-    for (let j = 0; j < i - 1; j++) {
-      console.log("222 " + arr, arr[j], arr[j + 1]);
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
-    }
-  }
-  return arr;
-}
-
-console.log(bubbleSort1([37, 45, 29, 8, 88, 100]));
+// function bubbleSort(arr) {
+//   const swap = (arr, idx1, idx2) => {
+//     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+//   };
+//   for (let i = arr.length; i > 0; i--) {
+//     for (let j = 0; j < i - 1; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         swap(arr, j, j + 1);
+//       }
+//     }
+//   }
+//   return arr;
+// }
 
 function bubbleSort(arr) {
   const swap = (arr, idx1, idx2) => {
     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
   };
-
-  for (let i = arr.length; i > 0; i--) {
-    for (let j = 0; j < i - 1; j++) {
-      console.log(arr, arr[j], arr[j + 1]);
-      if (arr[j] > arr[j + 1]) {
-        swap(arr, j, j + 1);
-      }
-    }
-  }
-  return arr;
-}
-
-console.log(bubbleSort([37, 45, 29, 8, 88, 100]));
-
-function bubbleSort2(arr) {
-  const swap = (arr, idx1, idx2) => {
-    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
-  };
-
   let noSwaps;
   for (let i = arr.length; i > 0; i--) {
     noSwaps = true;
     for (let j = 0; j < i - 1; j++) {
-      console.log("test " + arr, arr[j], arr[j + 1]);
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1);
         noSwaps = false;
@@ -68,4 +56,4 @@ function bubbleSort2(arr) {
   return arr;
 }
 
-console.log(bubbleSort2([37, 45, 29, 8, 88, 100]));
+bubbleSort([37, 45, 29, 8]);
